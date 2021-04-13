ad26372fb0d67dce3f8071ef0fbbfcc6cce10cdf
Sven Krefeld <info@svenkrefeld.de>
2021-04-13 14:48:32 +0200

    Merge upstream, update to shopware v6.3.5.3

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#set -o xtrace
 trap 'echo -e "\e[0;32m" && echo -ne $(date "+%Y-%m-%d %H:%M:%S") && echo " >> Executing: $BASH_COMMAND" && echo -e "\e[0m"' DEBUG
 mysql -h127.0.0.1 -uroot -e "CREATE DATABASE application;"
 composer install --no-interaction --optimize-autoloader
@@ -10,4 +9,5 @@ composer install --no-interaction --optimize-autoloader
 # setup the environment
 APP_URL=http://127.0.0.1 bin/console assets:install
 APP_URL=http://127.0.0.1 bin/console system:setup
+
 trap - DEBUG
